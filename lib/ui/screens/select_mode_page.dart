fe49f56f0826d3765552c083b63bfdac6a64038c
Luan Gibin <luan.gibin77@gmail.com>
2026-02-17 14:25:29 -0300

    feat: select_mode_page created

---
+++ lib/ui/screens/select_mode_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectModePage extends StatelessWidget {
+  const SelectModePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Select Mode Page")));
+  }
+}
